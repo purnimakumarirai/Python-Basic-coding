68a8aa84c087107ca0aa0a5d95f4aff6d86073a5
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2022-09-19 20:39:16 +0530

    Create LastElementInList.py

---
+++ LastElementInList.py
@@ -0,0 +1,7 @@
+#last element in list
+
+apple=['iphone11','iphone12','mac']                                                                                                     
+print(apple[-1][-1]) 
+
+
+#o/p:- c
