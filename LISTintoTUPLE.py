db3e8c7370360eb00128f78a732aeb955bb05c4a
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2022-09-19 20:35:46 +0530

    Create LISTintoTUPLE.py

---
+++ LISTintoTUPLE.py
@@ -0,0 +1,7 @@
+#List into tuple
+
+apple=['iphone11','iphone12','mac']                                                                                                      
+listastuple= tuple(apple)                                                                                                                 
+print(listastuple)
+
+#o/p:- ('iphone11','iphone12','mac')
