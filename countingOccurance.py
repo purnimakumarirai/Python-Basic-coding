5ef3cbf40b8da63790073cf19b4389074cb578d9
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2022-09-19 20:37:38 +0530

    Create countingOccurance.py

---
+++ countingOccurance.py
@@ -0,0 +1,8 @@
+#counting occurance
+
+apple=['iphone11','iphone12','mac']                                                                                               
+print(apple.count('iphone11'))
+
+
+
+#o/p:- 1
