fd14c2c05b60ed1b73362b3d6d978dcc0dc9ebb6
Purnima kumari <35552148+purnimakumarirai@users.noreply.github.com>
2022-09-19 20:32:15 +0530

    Create LISTintoSTRING.py

---
+++ LISTintoSTRING.py
@@ -0,0 +1,5 @@
+#List into string
+
+apple=['iphone11','iphone12','mac']                                                                                                      
+listasstring= ''.(apple) 
+print(listasstring)
